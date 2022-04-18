4eb9d607d4a18b2ec4f78b3dd9a376a83277f4be
Yan Mingzhi <realraindust@gmail.com>
2022-04-18 17:01:22 +0800

    change error code into enum

--- examples/codectest.rs
+++ examples/codectest.rs
@@ -4,6 +4,7 @@ use std::io::Read;
 use std::io::Write;
 use structopt::clap::AppSettings;
 use structopt::StructOpt;
+use tea_codec::error::TeaResult;
 
 #[macro_use]
 extern crate serde_json;
@@ -17,172 +18,172 @@ use codec::Sample;
     name = "codectest", 
     about = "A CLI for generating and validating codec structs")]
 struct Cli {
-    #[structopt(flatten)]
-    command: CliCommand,
+	#[structopt(flatten)]
+	command: CliCommand,
 }
 
 #[derive(Debug, Clone, StructOpt)]
 enum CliCommand {
-    /// Validate a codec output file
-    #[structopt(name = "validate")]
-    Validate(ValidateCommand),
+	/// Validate a codec output file
+	#[structopt(name = "validate")]
+	Validate(ValidateCommand),
 
-    /// Generate a codec output file
-    #[structopt(name = "generate")]
-    Generate(GenerateCommand),
+	/// Generate a codec output file
+	#[structopt(name = "generate")]
+	Generate(GenerateCommand),
 }
 
 #[derive(Debug, Clone, StructOpt)]
 struct GenerateCommand {
-    /// Path of output file
-    #[structopt(short = "p", long = "path")]
-    path: String,
+	/// Path of output file
+	#[structopt(short = "p", long = "path")]
+	path: String,
 }
 
 #[derive(Debug, Clone, StructOpt)]
 struct ValidateCommand {
-    /// Path of file to validate
-    #[structopt(short = "p", long = "path")]
-    path: String,
+	/// Path of file to validate
+	#[structopt(short = "p", long = "path")]
+	path: String,
 }
 
-fn main() -> anyhow::Result<()> {
-    let args = Cli::from_args();
-    let cmd = args.command;
+fn main() -> TeaResult<()> {
+	let args = Cli::from_args();
+	let cmd = args.command;
 
-    match handle_command(cmd) {
-        Ok(_) => {}
-        Err(e) => {
-            println!("Command line failure: {}", e);
-        }
-    }
-    Ok(())
+	match handle_command(cmd) {
+		Ok(_) => {}
+		Err(e) => {
+			println!("Command line failure: {}", e);
+		}
+	}
+	Ok(())
 }
 
-fn handle_command(cmd: CliCommand) -> anyhow::Result<()> {
-    match cmd {
-        CliCommand::Validate(valcmd) => validate_file(&valcmd),
-        CliCommand::Generate(gencmd) => generate_file(&gencmd),
-    }
+fn handle_command(cmd: CliCommand) -> TeaResult<()> {
+	match cmd {
+		CliCommand::Validate(valcmd) => validate_file(&valcmd),
+		CliCommand::Generate(gencmd) => generate_file(&gencmd),
+	}
 }
 
-fn generate_file(cmd: &GenerateCommand) -> anyhow::Result<()> {
-    let output = json!({
-        "version" : codec::VERSION,
-        "httpserver": generate_httpserver_sample(),
-        "keyvalue": generate_keyvalue_sample(),
-        "blobstore": generate_blobstore_sample(),
-        "messaging": generate_messaging_sample(),
-        "extras": generate_extras_sample(),
-        "logging": generate_logging_sample(),
-        "eventstreams": generate_eventstreams_sample()
-    });
-    let mut buffer = File::create(&cmd.path)?;
-    buffer.write_all(&serde_json::to_vec(&output).unwrap())?;
-    buffer.flush()?;
-    Ok(())
+fn generate_file(cmd: &GenerateCommand) -> TeaResult<()> {
+	let output = json!({
+		"version" : codec::VERSION,
+		"httpserver": generate_httpserver_sample(),
+		"keyvalue": generate_keyvalue_sample(),
+		"blobstore": generate_blobstore_sample(),
+		"messaging": generate_messaging_sample(),
+		"extras": generate_extras_sample(),
+		"logging": generate_logging_sample(),
+		"eventstreams": generate_eventstreams_sample()
+	});
+	let mut buffer = File::create(&cmd.path)?;
+	buffer.write_all(&serde_json::to_vec(&output).unwrap())?;
+	buffer.flush()?;
+	Ok(())
 }
 
-fn validate_file(cmd: &ValidateCommand) -> anyhow::Result<()> {
-    let mut f = File::open(&cmd.path)?;
-    let mut buffer = Vec::new();
+fn validate_file(cmd: &ValidateCommand) -> TeaResult<()> {
+	let mut f = File::open(&cmd.path)?;
+	let mut buffer = Vec::new();
 
-    // read the whole file
-    f.read_to_end(&mut buffer)?;
-    let raw: serde_json::Value = serde_json::from_str(::std::str::from_utf8(&buffer)?)?;
+	// read the whole file
+	f.read_to_end(&mut buffer)?;
+	let raw: serde_json::Value = serde_json::from_str(::std::str::from_utf8(&buffer)?)?;
 
-    assert(
-        &raw["httpserver"]["request"],
-        codec::http::Request::sample(),
-    )?;
-    assert(
-        &raw["httpserver"]["response"],
-        codec::http::Response::sample(),
-    )?;
-    assert(
-        &raw["blobstore"]["filechunk"],
-        codec::blobstore::FileChunk::sample(),
-    )?;
-    assert(
-        &raw["blobstore"]["containerlist"],
-        codec::blobstore::ContainerList::sample(),
-    )?;
-    assert(
-        &raw["extras"]["result_guid"],
-        codec::extras::GeneratorResult::sample(),
-    )?;
-    assert(
-        &raw["eventstreams"]["streamquery"],
-        codec::eventstreams::StreamQuery::sample(),
-    )?;
-    assert(
-        &raw["keyvalue"]["setrequest"],
-        codec::keyvalue::SetRequest::sample(),
-    )?;
-    assert(
-        &raw["messaging"]["requestmessage"],
-        codec::messaging::RequestMessage::sample(),
-    )?;
-    assert(
-        &raw["logging"]["writelogrequest"],
-        codec::logging::WriteLogRequest::sample(),
-    )?;
-    println!("Valid!");
-    Ok(())
+	assert(
+		&raw["httpserver"]["request"],
+		codec::http::Request::sample(),
+	)?;
+	assert(
+		&raw["httpserver"]["response"],
+		codec::http::Response::sample(),
+	)?;
+	assert(
+		&raw["blobstore"]["filechunk"],
+		codec::blobstore::FileChunk::sample(),
+	)?;
+	assert(
+		&raw["blobstore"]["containerlist"],
+		codec::blobstore::ContainerList::sample(),
+	)?;
+	assert(
+		&raw["extras"]["result_guid"],
+		codec::extras::GeneratorResult::sample(),
+	)?;
+	assert(
+		&raw["eventstreams"]["streamquery"],
+		codec::eventstreams::StreamQuery::sample(),
+	)?;
+	assert(
+		&raw["keyvalue"]["setrequest"],
+		codec::keyvalue::SetRequest::sample(),
+	)?;
+	assert(
+		&raw["messaging"]["requestmessage"],
+		codec::messaging::RequestMessage::sample(),
+	)?;
+	assert(
+		&raw["logging"]["writelogrequest"],
+		codec::logging::WriteLogRequest::sample(),
+	)?;
+	println!("Valid!");
+	Ok(())
 }
 
 fn generate_httpserver_sample() -> serde_json::Value {
-    json!({
-        "request": base64::encode(codec::serialize(&codec::http::Request::sample()).unwrap()),
-        "response": base64::encode(codec::serialize(&codec::http::Response::sample()).unwrap())
-    })
+	json!({
+		"request": base64::encode(codec::serialize(&codec::http::Request::sample()).unwrap()),
+		"response": base64::encode(codec::serialize(&codec::http::Response::sample()).unwrap())
+	})
 }
 
 fn generate_keyvalue_sample() -> serde_json::Value {
-    json!({
-        "setrequest": base64::encode(codec::serialize(&codec::keyvalue::SetRequest::sample()).unwrap())
-    })
+	json!({
+		"setrequest": base64::encode(codec::serialize(&codec::keyvalue::SetRequest::sample()).unwrap())
+	})
 }
 
 fn generate_blobstore_sample() -> serde_json::Value {
-    json!({
-        "filechunk": base64::encode(codec::serialize(&codec::blobstore::FileChunk::sample()).unwrap()),
-        "containerlist": base64::encode(codec::serialize(&codec::blobstore::ContainerList::sample()).unwrap())
-    })
+	json!({
+		"filechunk": base64::encode(codec::serialize(&codec::blobstore::FileChunk::sample()).unwrap()),
+		"containerlist": base64::encode(codec::serialize(&codec::blobstore::ContainerList::sample()).unwrap())
+	})
 }
 
 fn generate_messaging_sample() -> serde_json::Value {
-    json!({
-        "requestmessage": base64::encode(codec::serialize(&codec::messaging::RequestMessage::sample()).unwrap())
-    })
+	json!({
+		"requestmessage": base64::encode(codec::serialize(&codec::messaging::RequestMessage::sample()).unwrap())
+	})
 }
 
 fn generate_extras_sample() -> serde_json::Value {
-    json!({
-        "result_guid": base64::encode(codec::serialize(&codec::extras::GeneratorResult::sample()).unwrap()),
-    })
+	json!({
+		"result_guid": base64::encode(codec::serialize(&codec::extras::GeneratorResult::sample()).unwrap()),
+	})
 }
 
 fn generate_logging_sample() -> serde_json::Value {
-    json!({
-        "writelogrequest": base64::encode(codec::serialize(&codec::logging::WriteLogRequest::sample()).unwrap()),
-    })
+	json!({
+		"writelogrequest": base64::encode(codec::serialize(&codec::logging::WriteLogRequest::sample()).unwrap()),
+	})
 }
 
 fn generate_eventstreams_sample() -> serde_json::Value {
-    json!({
-        "streamquery": base64::encode(codec::serialize(&codec::eventstreams::StreamQuery::sample()).unwrap())
-    })
+	json!({
+		"streamquery": base64::encode(codec::serialize(&codec::eventstreams::StreamQuery::sample()).unwrap())
+	})
 }
 
 fn assert<'de, T: Deserialize<'de> + PartialEq + std::fmt::Debug>(
-    value: &serde_json::Value,
-    expected: T,
-) -> anyhow::Result<()> {
-    let encoded = value.to_string().replace("\"", "");
-    let bytes = base64::decode(&encoded)?;
+	value: &serde_json::Value,
+	expected: T,
+) -> TeaResult<()> {
+	let encoded = value.to_string().replace("\"", "");
+	let bytes = base64::decode(&encoded)?;
 
-    let val: T = codec::deserialize(&bytes)?;
-    assert_eq!(val, expected);
-    Ok(())
+	let val: T = codec::deserialize(&bytes)?;
+	assert_eq!(val, expected);
+	Ok(())
 }
