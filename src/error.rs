75dad25fb2dc1a265edab50bfa00e8efc913db84
AlseinX <xyh951115@live.com>
2022-08-31 01:10:04 +0800

    feat: add error codes

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,26 @@ use tea_codec::define_scope;
 
 define_scope! {
 	WasccCodec {
-
+		GeneralWascc;
+		BadDispatch;
+		GeneralHost;
+		NoSuchFunction;
+		WasmMisc;
+		HostCallFailure;
+		GuestCallFailure;
+		WapcGeneral;
+		WascapGeneral;
+		HostAuthorization;
+		CapabilityProvider;
+		MiscHost;
+		Plugin;
+		Middleware;
+		ActorToActorCallNotExist;
+		Invocation;
+		KeyValue;
+		Messaging;
+		EnvVar;
+		DiscardMessage;
+		ProviderOperationRejected;
 	}
 }
